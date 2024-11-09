b558f4c9425f406f11ff3afd7f516f8b07404926
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2024-11-09 18:18:04 -0100

    multithreaded the cracking

--- source/main.cpp
+++ source/main.cpp
@@ -8,22 +8,28 @@
 #include <iomanip>
 #include <openssl/sha.h> // vcpkg install openssl,, vcpkg integrate install // libssl.lib libcrypto.lib
 #include <openssl/evp.h>
+#include <thread>
 
 std::string sha256(const std::string& str);
+void crackPassword(std::vector<std::string> list, std::string_view hash);
+bool running{ false };
 
 int main(int argc, char* argv[])
 {
 	if (argc != 3)
 	{
 		printf("Usage: hashcracker.exe <HASH> <LIST>");
+		return 1;
 	}
 
-	std::string hash{ argv[1] }; // hash to crack
-	const char* list{ argv[2] }; // list location C:\\Users\\schwarztoter\\source\\repos\\hashcracker\\build\\passwords.txt
+	std::string hash{ argv[1]}; // hash to crack
+	const char* list{ argv[2]}; // list location C:\\Users\\schwarztoter\\source\\repos\\hashcracker\\build\\passwords.txt
 
 	std::ifstream inputFile(list); // location of password list.
 
 	std::vector<std::string> passwords; // vector of strings to store password list in.
+	std::vector<std::string> passwords1;
+	std::vector<std::string> passwords2;
 	std::string buffer;
 
 	if (!inputFile)
@@ -32,24 +38,78 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	int black{ 0 };
+
 	while (std::getline(inputFile, buffer)) // gets each line within password list and stores them into the passwords.
 	{
-		passwords.push_back(buffer);
-	}
-
-	inputFile.close(); // closes input file, becasue we dont need it anymore.
-
-	for (const auto& password : passwords) // loops through vector printing out all passwords allocated to it.
-	{
-		printf("Cracking password trying: %s\n", password.c_str());
-		if (hash.compare(sha256(password)) == 0)
+		if (black == 0)
 		{
-			printf("Password cracked: %s\n", password.c_str());
-			break;
+			passwords.push_back(buffer);
+			black++;
+		}
+		else if (black == 1)
+		{
+			passwords1.push_back(buffer);
+			black++;
+		}
+		else if (black == 2)
+		{
+			passwords2.push_back(buffer);
+			black++;
+		}
+		else
+		{
+			black = 0;
 		}
 	}
 
+	//size_t middle = passwords.size() / 2;
+
+	/*std::vector<std::string> passwords1(passwords.begin(), passwords.begin() + middle);
+	std::vector<std::string> passwords2(passwords.begin() + middle, passwords.end());
+	memset(&passwords, 0, sizeof(passwords));*/
+
+	inputFile.close(); // closes input file, becasue we dont need it anymore.
+
+	printf("Cracking password...\n");
+
+	std::thread crack1(crackPassword, passwords , hash);
+	std::thread crack2(crackPassword, passwords1, hash);
+	std::thread crack3(crackPassword, passwords2, hash);
+	
+	crack1.detach();
+	crack2.detach();
+	crack3.detach();
+
+	int time = 0;
+	running = true;
+
+	while (running)
+	{
+		if ((time % 5) == 0)
+		{
+			std::cout << "Time elapsed: " << time << "seconds" << '\n';
+		}
+		Sleep(1000);
+		time++;
+	}
+	
+	printf("Time to crack: %dseconds", time);
+
 	return 0;
+}
+
+void crackPassword(std::vector<std::string> list, std::string_view hash)
+{
+	for (const auto& password : list) // loops through vector printing out all passwords allocated to it.
+	{
+		if (hash.compare(sha256(password)) == 0)
+		{
+			printf("Password cracked: %s\n", password.c_str());
+			running = false;
+			break;
+		}
+	}
 }
 
 std::string sha256(const std::string& str)
